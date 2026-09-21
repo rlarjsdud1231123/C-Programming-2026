75a37e36042131bbcc4ae6b4769f21c57cd5278c
rlarjsdud1231123 <anyfeel33@gmail.com>
2026-09-21 14:59:12 +0900

    week-04 연산자실습자료
    15번 초를 시,분,초 16번 합격 여부 판단 17번 EXEC 권한 추가

--- main.c
+++ main.c
@@ -1,72 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <windows.h> 
+#include <windows.h>
 
-#define PI 3.141592653589793
-#define INCH_TO_CM 2.54
+#define READ 0x01
+#define WRITE 0x02
+#define EXEC 0x04 
 
-void exercl(void) {
-    double inch, cm;
-    printf("인치(inch) 입력: ");
-    if (scanf("%lf", &inch) != 1) {
-        printf("잘못된 입력입니다.\n\n");
-        while (getchar() != '\n');
-        return;
-    }
-    cm = inch * INCH_TO_CM;
-    printf("%.2f 인치 = %.2f cm\n\n", inch, cm);
+void exercl(void){
+	int total = 7384;
+	int hours = total / 3600;
+	int minutes = (total % 3600) / 60;
+	int seconds = total % 60;
+	
+	printf("[15번] %d초 -> %d 시간 %d분 %d초\n", total, hours, minutes, seconds);
 }
-
 void exercl2(void) {
-    double C, F;
-    printf("섭씨(C) 입력: ");
-    if (scanf("%lf", &C) != 1) {
-        printf("잘못된 입력입니다.\n\n");
-        while (getchar() != '\n');
-        return;
-    }
-    F = (C * 1.8) + 32;
-    printf("%.2f ℃ = %.2f ℉\n\n", C, F);
+	int score = 75;
+	int attendance = 85;
+	int passed = (score >= 60) && (attendance >= 80);
+	
+	printf("[16번] 점수: %d, 출석률: %d%% -> 합격여부: %s(%d)\n",
+		score, attendance, passed ? "합격" : "불합격", passed);
 }
-
-void exercl3(void) {
-    double r, area;
-    printf("반지름(r) 입력: ");
-    if (scanf("%lf", &r) != 1) {
-        printf("잘못된 입력입니다.\n\n");
-        while (getchar() != '\n');
-        return;
-    }
-    area = r * r * PI;
-    printf("반지름 %.2f -> 넓이 = %.2f\n\n", r, area);
+void exercl3(void){
+	unsigned int permission = READ | WRITE;
+	permission |= EXEC;
+	printf("[17번] 최종 권한 값: 0x%02X\n", permission);
 }
-
 int main() {
-    SetConsoleOutputCP(65001); 
-
-    int sel;
-
-    while (1) {
-        printf("1.인치변환  2.온도변환  3.원의넓이  0.종료\n선택: ");
-
-        if (scanf("%d", &sel) != 1) {
-            printf("\n잘못된 입력입니다. 숫자를 입력하세요.\n\n");
-            while (getchar() != '\n');
-            continue;
-        }
-
-        if (sel == 0) {
-            printf("프로그램을 종료합니다.\n");
-            break;
-        }
-
-        printf("\n");
-
-        if (sel == 1) exercl();
-        else if (sel == 2) exercl2();
-        else if (sel == 3) exercl3();
-        else printf("잘못된 메뉴 번호입니다.\n\n");
-    }
-
-    return 0;
+	SetConsoleOutputCP(65001);
+	exercl();
+	exercl2();
+	exercl3();
 }
